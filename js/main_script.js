c9ecfd22d890639fa3bdd7fe57bf0f4bda8c5b1f
bloess <josua.bloess@uni-bayreuth.de>
2020-04-08 17:36:21 +0200

    version 0.1 finished

--- js/main_script.js
+++ js/main_script.js
@@ -47,20 +47,46 @@ function requestArrayBuffer(url) {
   });
 }
 
-function addImageToDoc(doc, name){
-	var myPromise = request('https://db.ygoprodeck.com/api/v6/cardinfo.php?name=' + name)
-	.then(function (result){
-		var data = JSON.parse(result);
-		console.log('requesting result');
-		console.log(data);
-		return requestArrayBuffer(data[0].card_images[0].image_url);
-	})
-	.then(function (res){
-		console.log('image: ');
-		console.log(res);
-		return doc.image(res);
-	});
-	return myPromise;
+var imagePos = 0;
+
+function addImageToDoc(doc){
+	return (img_url)=>{
+			console.log('image: ');
+			console.log(img_url);
+			var xPos = imagePos%3;
+			var yPos = Math.floor(imagePos/3);
+			imagePos = (imagePos + 1);	
+			doc.image(img_url, 10 + xPos * 167, 10 + yPos * 243, {width: 166});
+			if(imagePos >= 9)
+			{
+				doc.addPage();
+				imagePos = 0;
+			}
+	};
+}
+
+function getImageUrlFromID(id){
+	return ()=>{
+		return request('https://db.ygoprodeck.com/api/v6/cardinfo.php?id=' + id)
+		.then(function (result){
+			var data = JSON.parse(result);
+			console.log('requesting result');
+			console.log(data);
+			return requestArrayBuffer(data[0].card_images[0].image_url);
+		});
+	};
+}
+
+function getImageUrlFromName(name){
+	return ()=>{
+		return request('https://db.ygoprodeck.com/api/v6/cardinfo.php?name=' + name)
+		.then(function (result){
+			var data = JSON.parse(result);
+			console.log('requesting result');
+			console.log(data);
+			return requestArrayBuffer(data[0].card_images[0].image_url);
+		});
+	};
 }
 
 function getLines(){
@@ -68,7 +94,23 @@ function getLines(){
 }
 
 function generateProxies(){
+	
+	// create a document the same way as above
+	const doc = new PDFDocument;
+
+	// pipe the document to a blob
+	const stream = doc.pipe(blobStream());
+	stream.on('finish', function() {
+		/*var iframe = document.querySelector('iframe');*/
+		  // get a blob you can do whatever you like with
+		const blob = stream.toBlob('application/pdf');
+		saveAs(blob, "download.pdf");	 
+	});
+
+
 	var lines = getLines();
+	var overallProcess = Promise.resolve();
+	
 	for(var i = 0; i < lines.length; i++){
 		if(/^\/\//.test(lines[i]) || /^#/.test(lines[i])){
 			console.log("skipping comment " + lines[i]);
@@ -85,11 +127,15 @@ function generateProxies(){
 			//console.log(regex_result);
 			console.log("number: " + number);
 			var id_regex_result = /^[0-9]{7}$/.exec(regex_result[2]);
-			if(id_regex_result){
-				console.log("found id: " + regex_result[2]);
-			}
-			else{
-				console.log("found name: " + regex_result[2]);
+				for(var j = 0; j < number; j++){
+				if(id_regex_result){
+					console.log("found id: " + regex_result[2]);
+					overallProcess = overallProcess.then(getImageUrlFromID(regex_result[2])).then(addImageToDoc(doc));
+				}
+				else{
+					console.log("found name: " + regex_result[2]);
+					overallProcess = overallProcess.then(getImageUrlFromName(regex_result[2])).then(addImageToDoc(doc));
+				}
 			}
 		}
 		else{
@@ -97,27 +143,8 @@ function generateProxies(){
 		}
 		
 	}
-
-
-	// create a document the same way as above
-	const doc = new PDFDocument;
-
-	// pipe the document to a blob
-	const stream = doc.pipe(blobStream());
-	stream.on('finish', function() {
-		/*var iframe = document.querySelector('iframe');*/
-		  // get a blob you can do whatever you like with
-		const blob = stream.toBlob('application/pdf');
-		saveAs(blob, "download.pdf");	 
-	});
-
-
-	// Embed a font, set the font size, and render some text
-
-	// Finalize PDF file
-	var buildTask = new Promise(function(){});
 	
-	buildTask = buildTask
+	overallProcess = overallProcess
 	.then(function(){return addImageToDoc(doc,'Tornado%20Dragon');})
 		.then(function(){return addImageToDoc(doc,'Dark%20Magician');})
 		.then(function(){doc.end();})
